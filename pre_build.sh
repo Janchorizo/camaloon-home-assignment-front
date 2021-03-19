1b5c453036662f13293b6a00d9e5d4eeeabb1947
Janchorizo <jancho@usal.es>
2021-03-19 13:56:22 +0100

    Add prebuild script.

---
+++ pre_build.sh
@@ -0,0 +1,8 @@
+mkdir -p dist
+echo "Clearing previous build ..."
+rm -fr dist/*
+echo "Copying static files ..."
+cp -r static/vendor dist
+cp _redirects dist
+cp -r static/assets dist
+cp static/index_prod.html dist/index.html

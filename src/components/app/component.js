e06500da0ff1ec1a8983c77211dcbf8f80670b25
Janchorizo <jancho@usal.es>
2021-03-19 14:02:32 +0100

    Add basic React app implementation.

---
+++ src/components/app/component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// internal
+import style from './style.module.css';
+
+
+/**
+ * App root component
+ * @component
+ * @return {React.Component}
+ */
+export default function App() {
+  return <div id={style.app}>
+    <h1>Camaloon home assignment</h1>
+    <h2>Alejandro Rodríguez Díaz</h2>
+    <span>
+      Contact me at: <a href='mailto:jancho@usal.es'>jancho@usal.es</a>
+    </span>
+    <p>
+
+    </p>
+  </div>;
+}

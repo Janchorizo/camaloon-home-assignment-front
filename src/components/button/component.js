0dd6eb1c073f5f5f6ac43a1914d30c072d9bb2a4
Janchorizo <jancho@usal.es>
2021-03-23 18:45:43 +0100

    Add button component.

---
+++ src/components/button/component.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// internal
+import style from './style.module.css';
+
+
+/**
+ * Section component
+ * @component
+ * @return {React.Component}
+ */
+export default function Button({
+  filled=false,
+  textSizeClass='text-medium',
+  onClick=null,
+  children,
+}) {
+  const cssClasses = [
+    style.button,
+    textSizeClass,
+    filled === true ? style.filled : '',
+  ].join(' ');
+
+  return <button className={cssClasses} onClick={() => onClick?.()}>
+    {children}
+  </button>;
+}
+
+Button.propTypes = {
+  filled: PropTypes.bool,
+  textSizeClass: PropTypes.string,
+  onClick: PropTypes.func,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+

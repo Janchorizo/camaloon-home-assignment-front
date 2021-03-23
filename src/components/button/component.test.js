1ead6d6b6b42746a2997fa4d5d2c7637dce7a648
Janchorizo <jancho@usal.es>
2021-03-23 19:28:34 +0100

    Lint button component.

--- src/components/button/component.test.js
+++ src/components/button/component.test.js
@@ -24,19 +24,19 @@ test('button content is rendered', () => {
   expect(button.children().html()).toEqual('<span>some text</span>');
 });
 test('default text size CSS class is added', () => {
-  let wrapper = mount(
+  const wrapper = mount(
       <Button></Button>,
   );
 
-  let button = wrapper.find('button');
+  const button = wrapper.find('button');
   expect(button.hasClass('text-medium')).toBe(true);
 });
 test('provided text size CSS class is added', () => {
-  let wrapper = mount(
+  const wrapper = mount(
       <Button textSizeClass='text-large'></Button>,
   );
 
-  let button = wrapper.find('button');
+  const button = wrapper.find('button');
   expect(button.hasClass('text-large')).toBe(true);
 });
 test('extra "style.filled" CSS class is added for filled buttons', () => {
@@ -46,10 +46,10 @@ test('extra "style.filled" CSS class is added for filled buttons', () => {
 
   let button = wrapper.find('button');
   let buttonClasses = wrapper
-    .find('button')
-    .prop('className')
-    .trim()
-    .split(' ');
+      .find('button')
+      .prop('className')
+      .trim()
+      .split(' ');
   expect(buttonClasses.length).toEqual(2);
   expect(button.hasClass('button')).toBe(true);
   expect(button.hasClass('text-medium')).toBe(true);
@@ -60,10 +60,10 @@ test('extra "style.filled" CSS class is added for filled buttons', () => {
 
   button = wrapper.find('button');
   buttonClasses = wrapper
-    .find('button')
-    .prop('className')
-    .trim()
-    .split(' ');
+      .find('button')
+      .prop('className')
+      .trim()
+      .split(' ');
   expect(buttonClasses.length).toEqual(3);
   expect(button.hasClass('button')).toBe(true);
   expect(button.hasClass('filled')).toBe(true);
@@ -72,7 +72,9 @@ test('extra "style.filled" CSS class is added for filled buttons', () => {
 test('Calls the provided callback function', () => {
   const textState = {called: false};
   const wrapper = mount(
-      <Button onClick={() => {textState.called = true;}}>
+      <Button onClick={() => {
+        textState.called = true;
+      }}>
         <p>Some sample text</p>
       </Button>,
   );

0fb8695862011d65315eb1aca78caffde403d553
Janchorizo <jancho@usal.es>
2021-03-24 10:31:55 +0100

    Add two thirds layout component.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export {default as App} from './app';
 export {default as Section} from './section';
 export {default as Subsection} from './subsection';
+export {default as PageLayout} from './page_layout';
+export {default as Button} from './button';
+export {default as DoubleButton} from './double_button';
+export {default as TwoThirdsLayout} from './two_thirds_layout';
